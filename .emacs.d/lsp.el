6e881e54526f4f639e5baf60de205265f90ab02b
MaeBrooks <home_in_the_rain@proton.me>
2024-12-04 14:21:04 -0800

    removed all files to start over!

--- .emacs.d/lsp.el
+++
@@ -1,5 +0,0 @@
-(defun format-buffer ()
-  (unless (not 'eglot-format-buffer)
-    (funcall 'eglot-format-buffer)))
-
-(add-hook 'after-save-hook 'format-buffer)
--- .emacs.d/utilities.el
+++
@@ -1,4 +0,0 @@
-(defun not-nil (value) (not (eq value nil)))
-(defun list-filter (callback list) (seq-filter callback list))
-(defun list-filter-not (callback list) (seq-remove callback list))
-(defun list-map (callback list) (seq-map callback list))
