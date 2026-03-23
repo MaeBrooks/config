2d1bb5d88c37d0ee5146a90fc07b72ebd6954e17
Mae Brooks <home_in_the_rain+developer@proton.me>
2026-03-23 07:45:27 -0700

    More gpg work

--- .emacs.d/lisp/gpg.el
+++ .emacs.d/lisp/gpg.el
@@ -2,3 +2,14 @@
 
 (require 'epa-file)
 (epa-file-enable)
+
+;; (require 'pinentry)
+;; (pinentry-start)
+
+;; TODO: Can we hook on to vc's commit mode
+;; And have us enter the code before saving?
+;; - Look at vc code for when vc-next-action thingy causes summary message
+;; - Look at what is called for C-c C-c
+;; - Create a hook that is called right then / override the function here
+;;   So that we can call out to whatever is used for gpg-encrypt-region / gpg-descrypt-region
+
