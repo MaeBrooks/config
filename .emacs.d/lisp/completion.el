9d12ee530ddcd4adfb567b2dde4fc3d169837380
Mae Brooks <home_in_the_rain+developer@proton.me>
2026-03-23 07:44:40 -0700

    More gpg work

--- .emacs.d/lisp/completion.el
+++ .emacs.d/lisp/completion.el
@@ -1,8 +1,17 @@
 ;; -*- lexical-binding: t -*-
 
-;; This makes the menu buffer that shows at the bottom
+(require 'ido)
+
+;; This makes the mini buffer that shows at the bottom
 ;; of emacs for commands and such, show completions
 ;; vertically
 (require 'ido)
 (ido-mode t)
 (fido-vertical-mode t)
+
+;; This makes the completions that you get in the mini buffer
+;; work how everyone who isn't a power user would expect them to work
+(setq completion-styles '(emacs22 substring))
+(defun my-icomplete-styles () (setq-local completion-styles '(substring)))
+(add-hook 'icomplete-minibuffer-setup-hook 'my-icomplete-styles)
+
