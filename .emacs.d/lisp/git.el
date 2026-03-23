871e61abe106b81253edd2efed227dbd22e91c73
Mae Brooks <home_in_the_rain+developer@proton.me>
2026-03-23 07:45:15 -0700

    More gpg work

--- .emacs.d/lisp/git.el
+++ .emacs.d/lisp/git.el
@@ -1,6 +1,13 @@
 ;; -*- lexical-binding: t -*-
 
-;; TODO how the hell do we get git signing with vc-dir!
-;; OR, maybe make one exception for magit
+;; (require 'transient)
+;; VC-dir does not support rebasing
+;; It also doesnt seem to support signing commits with gpg / pinentry
+;; TODO Create a simple magit out of 'transient'
 
+;; TODO look at our normal config and steal some of the magit stuff there
+;; WE need to be able to use pinentry! But it doesnt seem to work!
+
+(require 'epg)
+;; This should work? but doesnt?
 (setq epg-pinentry-mode 'loopback)
