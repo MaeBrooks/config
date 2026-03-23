2aeedbef893e1541718f7c560989664b8567c6d8
Mae Brooks <home_in_the_rain+developer@proton.me>
2026-03-23 16:16:40 -0700

    Gave up on not using magit - its just too nice

--- .emacs.d/lisp/git.el
+++ .emacs.d/lisp/git.el
@@ -3,67 +3,19 @@
 ;; (require 'transient)
 
 ;; VC-dir (the built in emacs git handling package)
-;; does not support rebasing
-;; It also doesnt seem to support signing commits with gpg / pinentry
-;; (unless (locate-library "magit")
-;;  (package-install 'magit))
-;; (require 'magit)
+;; does not support rebasing or signing commits with gpg / pinentry
+;; This is the one exception to 'no external packages'
+;; Magit is basically an internal package anyways
+(unless (locate-library "magit")
+  (package-install 'magit t nil))
 
-(require 'transient)
+(require 'magit)
+(defun setup-magit ()
+  (setq project-switch-commands
+    '((project-find-file "Find file")
+       (project-find-regexp "Find regexp")
+       (project-find-dir "Find directory")
+       (magit-status "Magit" "m")
+       (project-eshell "Eshell"))))
 
-(package 'git)
-
-(term "git commit -m 'Hi mae'")
-
-(defun git-init ())
-(defvar git-init-function 'git-init)
-
-;; ;; ;;
-
-;; (defun git-commit-arguments nil (transient-args 'git-commit))
-
-(defun git-root ()
-  (interactive)
-  (let ((root (project-root (project-current))))
-    (when (not (stringp root))
-      (project-switch-project)
-      (setq root (project-root (project-current)))
-
-      (if (not (stringp root))
-	(error "No project found, unable to get git-root")))
-    root))
-
-(defun git-commit-create (&optional args)
-  "Create a new commit."
-  (interactive (list (git-commit-arguments)))
-  
-  (when (setq args (git-commit-assert args))
-    (let ((default-directory (git-root)))
-      (term-exec-1 "commit -m" ))))
-
-(defun tmp ()
-  (message "HAI"))
-
-(defun git--commit (&optional args)
-  "Just a quick commit function"
-  (interactive (list (transient-args transient-current-command)))
-  ;; Create scratch buffer, with new git-commit-mode
-  ;; in git-commit-mode, if C-c C-c is called,
-  (let ((msg "hai mae - from git--commit"))
-    (make-term "git-commit" (format "git commit -m '%s')" msg)
-      )
-    (message "%s" (list (transient-args transient-current-command))))
-  
-
-(transient-define-prefix git-commit ()
-  "Git commit"
-  ["Foo bar"
-    ("c" "create" git--commit)])
-
-(transient-define-prefix git-status ()
-  "Git status"
-  ["Commands"
-    ("c" "commit" git-commit)])
-
-
-
+(add-hook 'after-init-hook 'setup-magit)
