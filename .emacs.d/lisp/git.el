c0556b118049d772fa850034f302b7880ff26256
Mae Brooks <home_in_the_rain+developer@proton.me>
2026-03-23 12:42:03 -0700

    Hi mae

--- .emacs.d/lisp/git.el
+++ .emacs.d/lisp/git.el
@@ -1,13 +1,10 @@
 ;; -*- lexical-binding: t -*-
 
 ;; (require 'transient)
-;; VC-dir does not support rebasing
+
+;; VC-dir (the built in emacs git handling package)
+;; does not support rebasing
 ;; It also doesnt seem to support signing commits with gpg / pinentry
-;; TODO Create a simple magit out of 'transient'
-
-;; TODO look at our normal config and steal some of the magit stuff there
-;; WE need to be able to use pinentry! But it doesnt seem to work!
-
-(require 'epg)
-;; This should work? but doesnt?
-(setq epg-pinentry-mode 'loopback)
+(unless (locate-library "magit")
+  (package-install 'magit))
+(require 'magit)
