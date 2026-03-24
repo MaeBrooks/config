3f94389ad1485e8da31460500a4143139d7e49f7
Mae Brooks <home_in_the_rain+developer@proton.me>
2026-03-24 09:11:18 -0700

    Added quick toggle to C-h h instead of the 'how to say hello' that it shows by default

--- .emacs.d/lisp/keys.el
+++ .emacs.d/lisp/keys.el
@@ -3,3 +3,5 @@
 (global-set-key (kbd "M-v") (lambda () (interactive) (previous-line 6)))
 (global-set-key (kbd "C-v") (lambda () (interactive) (next-line 6)))
 
+(global-set-key (kbd "C-h h") 'help-quick-toggle)
+
